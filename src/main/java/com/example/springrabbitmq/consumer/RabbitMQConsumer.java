72ba86f2a7a968d0e67c210cb29a47648cb176bd
nhh0507 <nguyenhoanghai0507@gmail.com>
2024-05-20 15:40:45 +0700

    first commit

---
+++ src/main/java/com/example/springrabbitmq/consumer/RabbitMQConsumer.java
@@ -0,0 +1,16 @@
+package com.example.springrabbitmq.consumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+
+
+public class RabbitMQConsumer {
+
+    private static final Logger LOGGER  = LoggerFactory.getLogger(RabbitMQConsumer.class);
+
+    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
+    public void consume(String message){
+        LOGGER.info(String.format("Received message -> %s",message));
+    }
+}
