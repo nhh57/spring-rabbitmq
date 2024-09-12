f614dafeae649efc2cbaa23621825d7a6c94a0d8
nhh57 <nguyenhoanghai0507@gmail.com>
2024-09-12 09:47:13 +0700

    connect database spring 3

--- product-service/src/main/java/com/productservice/config/redis/RedisConfig.java
+++ product-service/src/main/java/com/productservice/config/redis/RedisConfig.java
@@ -32,17 +32,7 @@ public class RedisConfig {
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
     }
 
-//    @Bean
-//    @Primary
-//    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-//        // tạo ra một RedisTemplate
-//        // Với Key là Object
-//        // Value là Object
-//        // RedisTemplate giúp chúng ta thao tác với Redis
-//        RedisTemplate<Object, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(redisConnectionFactory);
-//        return template;
-//    }
+
 
     @Bean
     @Primary
